6ac82e34f0d82aaec711704c499aebfec358d3b4
Darshan <darshann679596@gmail.com>
2024-10-03 14:48:05 +0530

    Handled Test Cases

--- src/Components/StopWatch.jsx
+++ src/Components/StopWatch.jsx
@@ -1,40 +1,48 @@
-import React, { useEffect } from "react";
-import {useState} from "react";
+import React, { useEffect, useState } from "react";
+
 const StopWatch = () => {
-    const [data,setData] = useState(0);
-    const [flag,setFlag] = useState(false)
-    useEffect(()=>{
-        let interId;
-        if(flag){
-            interId = setInterval(() => {
-                setData((prevData)=>prevData + 1)
-            },1000) 
+    const [data, setData] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
+
+    useEffect(() => {
+        let intervalId;
+
+        if (isRunning) {
+            intervalId = setInterval(() => {
+                setData((prevData) => prevData + 1);
+            }, 1000);
+        } else {
+            clearInterval(intervalId);
         }
-        else{
-            clearInterval(interId)
-        }
-        return () => clearInterval(interId)
-    },[flag])
+
+        return () => clearInterval(intervalId);
+    }, [isRunning]);
+
     const formatTime = (seconds) => {
-        const minutes = Math.floor(seconds/60); 
-        const remainSeconds = seconds % 60;  
-        return `${minutes}:${remainSeconds < 10 ? "0" : ""}${remainSeconds}`
-    }
-    const handleStart = () => {
-        setFlag(!flag)
-    }
+        const minutes = Math.floor(seconds / 60);
+        const remainSeconds = seconds % 60;
+        return `${minutes}:${remainSeconds < 10 ? "0" : ""}${remainSeconds}`;
+    };
+
+    const handleStartStop = () => {
+        setIsRunning((prev) => !prev);
+    };
+
     const handleReset = () => {
-        setFlag(false)
+        setIsRunning(false);
         setData(0);
-    }
-    return(
+    };
+
+    return (
         <>
             <h1>Stop Watch</h1>
-            <p>Time : {formatTime(data)}</p>
-            <button styles = {{width : "45px" , height : "15px"}} onClick = {handleStart}>{flag ? "Stop" : "Start"}</button>&nbsp;
-            <button styles = {{width : "45px" , height : "15px"}} onClick={handleReset}>Reset</button>&nbsp;
-
+            <p>Time: {formatTime(data)}</p>
+            <button style={{ width: "45px", height: "15px" }} onClick={handleStartStop}>
+                {isRunning ? "Stop" : "Start"}
+            </button>&nbsp;
+            <button style={{ width: "45px", height: "15px" }} onClick={handleReset}>Reset</button>&nbsp;
         </>
-    )
-}
+    );
+};
+
 export default StopWatch;
